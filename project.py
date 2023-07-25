806d2ec2c707d09dda1eafaaf1bd9e0ac57360b1
Aleksey Chaban <aleksey.chaban@takeda.com>
2023-07-25 19:31:09 -0400

    Fixed interview and cleaned up user input

--- project.py
+++ project.py
@@ -2,6 +2,7 @@
 
 import argparse
 import re
+import sys
 
 
 def user_input():
@@ -54,42 +55,39 @@ def user_input():
     # Returns: User-Input as the object:  options
     return options
 
-
+# Requests manual input
 def interview():
     main_csv_path = input("Enter main CSV path: ")
     main_csv_path_final = main_csv(main_csv_path, True)
     
     source_csv_path = input("Enter source CSV path: ")
     source_csv_path_final = source_csv(source_csv_path, True)
-    
+
     report_xlsx_path = input("Enter report XLSX path: ")
     report_xlsx_path_final = report_xlsx(report_xlsx_path, True)
-    
+
     search_column = input("Enter common column: ")
     
     return main_csv_path_final, source_csv_path_final, report_xlsx_path_final, search_column
 
-def input_parser(file_path, file_which, file_type, file_api, interview_request_final):
-    if interview_request_final is True:
-        print(file_api)
-        print("testing1")
-        return file_api
+# Parses input
+def input_parser(file_path_parser, file_which, file_type, file_api, interview_request_final):
+    if interview_request_final is False:
+        return file_path_parser
     else:
-        while re.match(rf"\w*(.{file_type})$", file_path.lower()) is None:
+        while re.match(rf"\w*(.{file_type})$", file_path_parser.lower()) is None:
             print(f"Improper file. Format: *.{file_type}")
-            file_path = input(f"Enter {file_which} {file_type.upper()} path: ")
-            print("testing2")
-            print(file_path)
-            return file_path
+            file_path_parser = input(f"Enter {file_which} {file_type.upper()} path: ")
+        return file_path_parser
 
 def main_csv(file_path,interview_request = False):
-    input_parser(file_path, "main", "csv", api_options.main_csv, interview_request)
+    return input_parser(file_path, "main", "csv", api_options.main_csv, interview_request)
 
 def source_csv(file_path,interview_request = False):
-    input_parser(file_path, "source", "csv", api_options.source_csv, interview_request)
+    return input_parser(file_path, "source", "csv", api_options.source_csv, interview_request)
 
 def report_xlsx(file_path,interview_request = False):
-    input_parser(file_path, "report", "xlsx", api_options.main_csv, interview_request)
+    return input_parser(file_path, "report", "xlsx", api_options.report_xlsx, interview_request)
 
 # Returns common_column when called
 def common_column(interview_request = False):
@@ -101,16 +99,10 @@ def main():
     global api_options
     try:
         api_options = user_input()
-        if api_options.interview is True:
-            print(interview())
         if api_options.main_csv is not None:
-            main_csv()
-        if api_options.source_csv is not None:
-            source_csv()
-        if api_options.report_xlsx is not None:
-            report_xlsx()
-        if api_options.common_column is not None:
-            common_column()
+            print(main_csv(sys.argv[1:]))
+        else:
+            print(interview())
     except Exception as script_error:
         print({"Type": type(script_error), "Error": script_error})
 
