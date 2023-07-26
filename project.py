4a1946eccffc0f39c53b789f666cebd076697725
Aleksey Chaban <aleksey.chaban@takeda.com>
2023-07-26 09:15:34 -0400

    Request to push changes from Aleksey's repo (#4)
    * Removed some code and added empty print functions
    
    * Changed report_csv to report_xlsx
    
    * Changed another report_csv to report_xlsx
    
    * Added help menu
    
    * Added module os
    
    * remove module os
    
    * Sync code with current
    
    * Added regex
    
    * Fixed regex
    
    * Removed some code and added empty print functions
    
    * Changed report_csv to report_xlsx
    
    * Added help menu
    
    * Still having issues with git
    
    * Consolidated functions into input_parser
    
    * Testing new function
    
    * Resubmitting test items
    
    * Fixed interview and cleaned up user input
    
    * Fixed command line api
    
    * Changed interview to list and addressed edge case: not all args provided
    
    * Added comments
    
    * Fixed edge case: if .csv missing on file input in api request manual input
    
    * Consolidated file validation into function and fixed extra None value in list
    
    ---------
    
    Co-authored-by: Keisha Daniel <keisha.daniel@takeda.com>

--- project.py
+++ project.py
@@ -2,20 +2,13 @@
 
 import argparse
 import re
+import sys
 
 
 def user_input():
-    # Command-line parameters
-    # Code sourced from Eduardo Valdes
+    """Command-line parameters"""
+    # user_input sourced from Eduardo Valdes
     parser = argparse.ArgumentParser( description='User-Input parameters' )
-    parser.add_argument(
-        '--interview',
-        '-i',
-        const=True,
-        dest='interview',
-        nargs='?',
-        help='Calls the interview interface'
-    )
     parser.add_argument(
         '--main_csv',
         '-m',
@@ -27,6 +20,7 @@ def user_input():
     parser.add_argument(
         '--source_csv',
         '-s',
+        nargs='+',
         dest='source_csv',
         type=str,
         default=None,
@@ -51,82 +45,91 @@ def user_input():
     # Aggregating all User-Input parameters
     options = parser.parse_args()
     
-    # Returns: User-Input as the object:  options
+    # Returns: User-Input as the object: options
     return options
 
-
 def interview():
+    """Requests manual input"""
     main_csv_path = input("Enter main CSV path: ")
-    main_csv_path = main_csv(main_csv_path, True)
+    main_csv_path_final = main_csv(main_csv_path, True)
     
     source_csv_path = input("Enter source CSV path: ")
-    source_csv_path = source_csv(source_csv_path, True)
-    
+    source_csv_path_final = source_csv(source_csv_path, True)
+
     report_xlsx_path = input("Enter report XLSX path: ")
-    report_xlsx_path = report_xlsx(report_xlsx_path, True)
-    
+    report_xlsx_path_final = report_xlsx(report_xlsx_path, True)
+
     search_column = input("Enter common column: ")
     
-    return main_csv_path, source_csv_path, report_xlsx_path, search_column
-    
-    exit()
+    return main_csv_path_final, source_csv_path_final, report_xlsx_path_final, search_column
 
-def input_parser(file_which, file_type, file_spelled, file_api, interview_request_final):
+def file_validation(file_checked, file_which_checked,file_type_checked, file_wrong = "."):
+    """Ensures proper file input format"""
+    while re.match(rf"\w*(.{file_type_checked})$", file_checked.lower()) is None:
+        print(f"Improper file{file_wrong} Format: *.{file_type_checked}")
+        file_checked = input(f"Enter {file_which_checked} {file_type_checked.upper()} path: ")
+        return file_checked
+
+# First input is file path,
+# Second input is main, source or report,
+# Third input is .csv or .xlsx,
+# Fourth input flags if an interview is required.
+
+# If interview flag does not exist create an item list,
+# and if file path is a list confirm file type matches, append new list and return new list,
+# or if file path is not a list confirm file type matches and return file path.
+
+# Else if interview flag does exist,
+# Continue requesting file path until valid file path is provided and return file path.
+
+def input_parser(file_path_parser, file_which, file_type, interview_request_final):
+    """Returns list of file paths"""
     if interview_request_final is False:
-        return file_api
+        item_list = []
+        if type(file_path_parser) is list:
+            for item in file_path_parser:
+                if re.match(rf"\w*(.{file_type})$", item.lower()) is not None:
+                    item_list.append(item)
+                item_list.append(file_validation(item, file_which, file_type, f": {item}."))
+            item_list.remove(None)
+            return item_list
+        if re.match(rf"\w*(.{file_type})$", file_path_parser.lower()) is not None:
+            return file_path_parser
+        file_path_parser = file_validation(item, file_which, file_type)
+        return file_path_parser
     else:
-        while re.match(rf"\w*(.{file_type})$", path_function.lower()) is None:
-            print(f"Improper file. Format: *.{file_type}")
-            path_function = input(f"Enter {file_which} {file_spelled} path: ")
-            return path_function
+        file_path_parser = file_validation(item, file_which, file_type)
+        return file_path_parser
 
-def main_csv(interview_request = False):
-    input_parser("main", "csv", "CSV", api_options.main_csv, interview_request)
+def main_csv(file_path,interview_request = False):
+    """Returns main_csv file when called"""
+    return input_parser(file_path, "main", "csv", interview_request)
 
-def source_csv(source_csv_path_function_re, interview_request = False):
-    if interview_request is False:
-        source_csv_path_function = api_options.source_csv
-        return source_csv_path_function
-    else:
-        while re.match(r"\w*(.csv)$", source_csv_path_function_re.lower()) is None:
-            print("Improper file. Format: *.csv")
-            source_csv_path_function_re = input("Enter main CSV path: ")
-            return source_csv_path_function_re
-    #exit()
+def source_csv(file_path,interview_request = False):
+    """Returns source_csv files when called"""
+    return input_parser(file_path, "source", "csv", interview_request)
 
-def report_xlsx(report_xlsx_path_function_re, interview_request = False):
-    if interview_request is False:
-        report_xlsx_path_function = api_options.report_xlsx
-        return report_xlsx_path_function
-    else:
-        while re.match(r"\w*(.xlsx)$", report_xlsx_path_function_re.lower()) is None:
-            print("Improper file. Format: *.xlsx")
-            report_xlsx_path_function_re = input("Enter main XLSX path: ")
-            return report_xlsx_path_function_re
-    #exit()
+def report_xlsx(file_path,interview_request = False):
+    """Returns report_xlsx file when called"""
+    return input_parser(file_path, "report", "xlsx", interview_request)
 
-# Returns common_column when called
-def common_column(interview_request = False):
-    search_column = api_options.common_column
+def common_column(search_column):
+    """Returns common_column when called"""
     return search_column
-    
-    exit()
 
 def main():
-    #  User-Input options
-    global api_options
+    """Calls user_input and returns result"""
     try:
         api_options = user_input()
-        if api_options.interview is True:
-            interview()
-        if api_options.main_csv is not None:
-            main_csv()
-        if api_options.source_csv is not None:
-            source_csv()
-        if api_options.report_xlsx is not None:
-            report_xlsx()
-        if api_options.common_column is not None:
-            common_column()
+        if api_options.main_csv and api_options.source_csv and api_options.report_xlsx and api_options.common_column is not None:
+            output_list = []
+            output_list.append(main_csv(api_options.main_csv))
+            output_list.append(source_csv(api_options.source_csv))
+            output_list.append(report_xlsx(api_options.report_xlsx))
+            output_list.append(common_column(api_options.common_column))
+            print(output_list)
+        else:
+            print(list(interview()))
     except Exception as script_error:
         print({"Type": type(script_error), "Error": script_error})
 
